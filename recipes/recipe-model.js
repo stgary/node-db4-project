2ed761c4672e25174374be79a6154868ff9920de
Stephen Gary <sgary0@protonmail.com>
2020-09-10 16:24:24 -0400

    models

---
+++ recipes/recipe-model.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+}
+
+function getRecipes() {
+
+}
+
+function getShippingList() {
+    
+}
+
+function getInstructions() {
+    
+}
