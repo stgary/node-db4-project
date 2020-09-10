bde99b6b510e46f4576537796a99b9fb5461ccc9
Stephen Gary <sgary0@protonmail.com>
2020-09-10 18:01:12 -0400

    updated routers

--- ingredients/ingredients-router.js
+++ ingredients/ingredients-router.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
-const Mod = require('./ingredients-model');
+const db = require('./ingredients-model');
 
 const router = express.Router();
 
 router.get('/:id/recipes', (req, res) => {
+    const { id } = req.params;
 
+    db.getRFI(id)
+        .then(recipes => {
+            res.status(200).json(recipes)
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'oops! something went wrong :('
+            })
+        });
 });
 
 module.exports = router;
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,19 +1,60 @@
 const express = require('express');
 
-const Mod = require('./recipe-model');
+const db = require('./recipe-model');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.getRecipes()
+        .then(recipe => {
+            res.json(recipe);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'oops! something went wrong :('
+            })
+        });
 });
 
 router.get('/:id/shoppingList', (req, res) => {
+    const { id } = req.params;
 
+    db.getShoppingList(id)
+        .then(list => {
+            if(list) {
+                res.json(list);
+            } else {
+                res.status(404).json({
+                    error: 'could not find the list with the specified ID :('
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'oops! something went wrong :('
+            })
+        });
+        
 });
 
 router.get('/:id/instructions', (req, res) => {
+    const { id } = req.params;
 
+    db.getInstructions(id)
+        .then(instructions => {
+            if(instructions) {
+                res.json(instructions);
+            } else {
+                res.status(404).json({
+                    error: 'could not find the instructions with the specified ID :('
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'oops! something went wrong :('
+            })
+        });
 });
 
 module.exports = router;
