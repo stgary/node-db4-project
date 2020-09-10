6c6210a07afb1d5ad6ae6c42ffea074711da04e8
Stephen Gary <sgary0@protonmail.com>
2020-09-10 17:48:01 -0400

    updated ingredients-model

--- ingredients/ingredients-model.js
+++ ingredients/ingredients-model.js
@@ -4,7 +4,10 @@ module.exports = {
     getRFI
 }
 
-function getRFI() {
-
+function getRFI(ingredient_id) {
+    db('ingredients')
+        .join('recipes', 'recipes.id', 'ingredients.recipe_id')
+        .select('recipe.recipe')
+        .where({ ingredients_id: ingredients_id });
 }
 
